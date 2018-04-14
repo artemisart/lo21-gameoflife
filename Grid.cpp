f3427188fe047243afc47b57fea76264dc9bffac
Mathis Chenuet <artdevelopp@hotmail.fr>
2018-04-14 18:22:26 +0200

    grids base implementation

--- Grid.cpp
+++ Grid.cpp
@@ -1,1 +1,11 @@
 #include "Grid.h"
+
+template<typename T>
+Grid1D<T>::Grid1D(std::size_t size) :
+    size(size)
+{}
+
+template<typename T>
+Grid2D<T>::Grid2D(std::size_t height, std::size_t width):
+    height(height), width(width)
+{}
--- Grid.h
+++ Grid.h
@@ -13,10 +13,11 @@ template<typename T>
 class Grid1D : Grid<T>
 {
 private:
+    std::size_t size;
     std::vector<T> values;
 
 public:
-    Grid1D();
+    Grid1D(std::size_t size);
 
     T getCell(int i);
     void setCell(int i, T value);
@@ -27,10 +28,11 @@ template<typename T>
 class Grid2D
 {
 private:
+    std::size_t height, width;
     std::vector<T> values;
 
 public:
-    Grid2D() {}
+    Grid2D(std::size_t height, std::size_t width);
 
     T getCell(int row, int col);
     void setCell(int row, int col, T value);
