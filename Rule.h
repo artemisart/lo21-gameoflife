bef226c740fe0d6d107e93f621ed74a47740f016
DSI <dsi@usec-utc.fr>
2018-04-29 19:00:56 +0200

    ajout rule.cpp et rule.h

--- Rule.h
+++ Rule.h
@@ -8,7 +8,7 @@ template<typename T, typename I>
 class Rule
 {
 public:
-	Rule();
+	virtual Rule();
 	virtual ~Rule() = 0;
 	virtual T calcNextState(const Grid<T,I> &grid, const I index) const = 0;
 };
@@ -20,18 +20,29 @@ private:
 	std::uint8_t num;
 
 public:
-	Rule1D(const std::uint8_t num);
-	bool calcNextState(const Grid<bool,Index1D> &grid, const Index1D index) const;
+	Rule1D();
+	~Rule1D();
+	Rule1D(const std::uint8_t i);
+	std::uint8_t getnum();
+	void setnum(const std::uint8_t i);
+	bool calcNextState(const Grid1D<bool,Index1D> &grid, const Index1D index) const;
 };
 
 
 class Rule2D : public Rule<bool, Index2D>
 {
 private:
-	std::uint8_t born, survive;
+	std::uint8_t born, survive; //born represente regles quand cell dead, survive quand cell lives 
 
 public:
-	Rule2D(const std::uint8_t born, const std::uint8_t survive);
+	Rule2D();
+	~Rule2D();
+	Rule2D(const std::uint8_t i, const std::uint8_t j);
+	std::uint8_t getborn();
+	std::uint8_t getsurvive();
+	void setborn(const std::uint8_t i);
+	void setsurvive(const std::uint8_t i);
+	bool calcNextState(const Grid2D<bool,Index2D> &grid, const Index2D index) const;
 };
 
 #endif // RULES_H
