c1cb1ea99691cefe509049b3f750271391f03cfd
Mathis Chenuet <artdevelopp@hotmail.fr>
2018-05-13 12:16:48 +0200

    clang-format

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,215 +1,201 @@
-#include"Automaton.h"
-#include"gameoflife.h"
-#include"Rule.h"
-#include"Grid.h"
-#include"History.h"
-#include<QString>
+#include <QString>
 
-Gameoflife::Gameoflife (QWidget* parent) : QWidget(parent) {
+#include "Automaton.h"
+#include "Grid.h"
+#include "History.h"
+#include "Rule.h"
+#include "gameoflife.h"
 
-    this->setFixedSize(600,300);
+Gameoflife::Gameoflife(QWidget* parent)
+	: QWidget(parent)
+{
+	this->setFixedSize(600, 300);
 
-    introduction = new QLabel("Bienvenue dans notre simulateur d'automate, choisissez l'automate que vous souhaitez simuler : ");
+	introduction = new QLabel("Bienvenue dans notre simulateur d'automate, choisissez l'automate que vous souhaitez simuler : ");
 
-    Automaton1D = new QPushButton("Automate 1D");
-    Automaton2D = new QPushButton("Automate 2D");
-    HexaAutomaton= new QPushButton("Automate Hexadécimal");
+	Automaton1D = new QPushButton("Automate 1D");
+	Automaton2D = new QPushButton("Automate 2D");
+	HexaAutomaton = new QPushButton("Automate Hexadécimal");
 
-    connect(Automaton1D, SIGNAL(clicked()),this, SLOT(Simulation_Automaton1D()) );
-    connect(Automaton2D, SIGNAL(clicked()),this, SLOT(Simulation_Automaton2D()) );
-    connect(HexaAutomaton, SIGNAL(clicked()),this, SLOT(Simulation_HexaAutomaton()) );
+	connect(Automaton1D, SIGNAL(clicked()), this, SLOT(Simulation_Automaton1D()));
+	connect(Automaton2D, SIGNAL(clicked()), this, SLOT(Simulation_Automaton2D()));
+	connect(HexaAutomaton, SIGNAL(clicked()), this, SLOT(Simulation_HexaAutomaton()));
 
+	presentation = new QHBoxLayout();
+	presentation->addWidget(Automaton1D);
+	presentation->addWidget(Automaton2D);
+	presentation->addWidget(HexaAutomaton);
 
-    presentation=new QHBoxLayout();
-    presentation->addWidget(Automaton1D);
-    presentation->addWidget(Automaton2D);
-    presentation->addWidget(HexaAutomaton);
+	presentation2 = new QVBoxLayout();
+	presentation2->addWidget(introduction);
+	presentation2->addLayout(presentation);
 
-    presentation2 = new QVBoxLayout();
-    presentation2->addWidget(introduction);
-    presentation2->addLayout(presentation);
-
-    setLayout(presentation2);
-
-
-
-
+	setLayout(presentation2);
 }
 
+void Gameoflife::Simulation_Automaton1D()
+{
+	Simulation = createNewWindow();
+	size = new QPushButton("Créer un automate de cette taille");
+	colField = new QLineEdit();
+	colField->setText("20");
 
-void Gameoflife::Simulation_Automaton1D(){
-  Simulation=createNewWindow();
-  size = new QPushButton("Créer un automate de cette taille");
-  colField=new QLineEdit();
-  colField->setText("20");
+	QString dimensionCol = colField->text();
+	bool ok = false;
+	int dim = dimensionCol.toInt(&ok);
 
-  QString dimensionCol=colField->text();
+	param = new QHBoxLayout();
 
-  bool ok = false;
-
-  int dim=dimensionCol.toInt(&ok);
-
-
-  param = new QHBoxLayout();
-
-  param->addWidget(colField);
-  param->addWidget(size);
-  param->addLayout(colone1);
+	param->addWidget(colField);
+	param->addWidget(size);
+	param->addLayout(colone1);
 
   unsigned int taille = 25;
 
-  depart=new QTableWidget(1,dim, Simulation);
-  depart->setFixedSize(dim*taille, taille);
-  depart->horizontalHeader()->setVisible(false);
-  depart->verticalHeader()->setVisible(false);
-  depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart = new QTableWidget(1, dim, Simulation);
+	depart->setFixedSize(dim * taille, taille);
+	depart->horizontalHeader()->setVisible(false);
+	depart->verticalHeader()->setVisible(false);
+	depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   for(unsigned int i=0; i<dim; i++){
-      depart->setColumnWidth(i, taille);
-      depart->setItem(0, i, new QTableWidgetItem(""));
-  }
+		depart->setColumnWidth(i, taille);
+		depart->setItem(0, i, new QTableWidgetItem(""));
+	}
 
-  grid=new QVBoxLayout();
-  grid->addLayout(param);
-  grid->addWidget(depart);
-  Simulation->setLayout(grid);
+	grid = new QVBoxLayout();
+	grid->addLayout(param);
+	grid->addWidget(depart);
+	Simulation->setLayout(grid);
 
-
-  connect(size, SIGNAL(clicked()), this, SLOT(setGrid1D()));
-
-
-
-
+	connect(size, SIGNAL(clicked()), this, SLOT(setGrid1D()));
 }
 
-void Gameoflife::Simulation_Automaton2D(){
-    Simulation = createNewWindow();
+void Gameoflife::Simulation_Automaton2D()
+{
+	Simulation = createNewWindow();
 
-    size = new QPushButton("Créer un automate de cette taille");
-    colField = new QLineEdit(Simulation);
-    rowField = new QLineEdit(Simulation);
-    colField->setText("20");
-    rowField->setText("20");
+	size = new QPushButton("Créer un automate de cette taille");
+	colField = new QLineEdit(Simulation);
+	rowField = new QLineEdit(Simulation);
+	colField->setText("20");
+	rowField->setText("20");
 
-    QString dimensionCol=colField->text();
-    QString dimensionRow=rowField->text();
+	QString dimensionCol = colField->text();
+	QString dimensionRow = rowField->text();
 
+	int dimCol = dimensionCol.toInt();
+	int dimRow = dimensionRow.toInt();
 
-    int dimCol=dimensionCol.toInt();
-    int dimRow=dimensionRow.toInt();
+	param = new QHBoxLayout();
 
+	param->addWidget(colField);
+	param->addWidget(rowField);
 
-    param = new QHBoxLayout();
-
-    param->addWidget(colField);
-    param->addWidget(rowField);
-
-    param->addWidget(size);
-    param->addLayout(colone1);
+	param->addWidget(size);
+	param->addLayout(colone1);
 
     unsigned int taille = 25;
 
-    depart=new QTableWidget(dimRow,dimCol, Simulation);
-    depart->setFixedSize(dimCol*taille, dimRow*taille);
-    depart->horizontalHeader()->setVisible(false);
-    depart->verticalHeader()->setVisible(false);
-    depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart = new QTableWidget(dimRow, dimCol, Simulation);
+	depart->setFixedSize(dimCol * taille, dimRow * taille);
+	depart->horizontalHeader()->setVisible(false);
+	depart->verticalHeader()->setVisible(false);
+	depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     for(unsigned int i=0; i<dimCol; i++){
         for(unsigned int j=0; j<dimRow; j++){
-            depart->setColumnWidth(i, taille);
-            depart->setRowHeight(j, taille);
-            depart->setItem(j, i, new QTableWidgetItem(""));
-        }
-    }
+			depart->setColumnWidth(i, taille);
+			depart->setRowHeight(j, taille);
+			depart->setItem(j, i, new QTableWidgetItem(""));
+		}
+	}
 
-    grid=new QVBoxLayout();
-    grid->addLayout(param);
-    grid->addWidget(depart);
-    Simulation->setLayout(grid);
+	grid = new QVBoxLayout();
+	grid->addLayout(param);
+	grid->addWidget(depart);
+	Simulation->setLayout(grid);
 
-
-    connect(size, SIGNAL(clicked()), this, SLOT(setGrid2D()));
+	connect(size, SIGNAL(clicked()), this, SLOT(setGrid2D()));
 }
 
-void Gameoflife::Simulation_HexaAutomaton(){
-    Simulation=createNewWindow();
-
+void Gameoflife::Simulation_HexaAutomaton()
+{
+	Simulation = createNewWindow();
 }
 
-QWidget* Gameoflife::createNewWindow(){
-    Simulation = new QWidget();
-    Simulation->show();
-    this->hide();
+QWidget* Gameoflife::createNewWindow()
+{
+	Simulation = new QWidget();
+	Simulation->show();
+	this->hide();
 
-    retour=new QPushButton("Retour au menu principal");
-    runSimulation = new QPushButton("Lancer la simulation");
-    next=new QPushButton("Calculer l'état suivant");
+	retour = new QPushButton("Retour au menu principal");
+	runSimulation = new QPushButton("Lancer la simulation");
+	next = new QPushButton("Calculer l'état suivant");
 
-    colone1=new QVBoxLayout();
-    colone1->addWidget(runSimulation);
-    colone1->addWidget(next);
-    colone1->addWidget(retour);
+	colone1 = new QVBoxLayout();
+	colone1->addWidget(runSimulation);
+	colone1->addWidget(next);
+	colone1->addWidget(retour);
 
-    connect(retour, SIGNAL(clicked()),this, SLOT(retour_menu()) );
-  //  connect(runSimulation, SIGNAL(clicked()), this, SLOT(run());
-   // connect(next, SIGNAL(clicked()), this, SLOT(next()));
+	connect(retour, SIGNAL(clicked()), this, SLOT(retour_menu()));
+	// connect(runSimulation, SIGNAL(clicked()), this, SLOT(run());
+	// connect(next, SIGNAL(clicked()), this, SLOT(next()));
 
-    return Simulation;
+	return Simulation;
 }
 
-void Gameoflife::retour_menu(){
-    this->show();
-    Simulation->hide();
+void Gameoflife::retour_menu()
+{
+	this->show();
+	Simulation->hide();
 }
 
+void Gameoflife::setGrid1D()
+{
+	QString dimensionCol = colField->text();
+	bool ok = false;
 
-void Gameoflife::setGrid1D(){
-    QString dimensionCol=colField->text();
-    bool ok = false;
+	int dimCol = dimensionCol.toInt(&ok);
 
-    int dimCol=dimensionCol.toInt(&ok);
-
-    depart->setColumnCount(dimCol);
+	depart->setColumnCount(dimCol);
 
     unsigned int taille = 25;
-    depart->setFixedSize(dimCol*taille, taille);
-    depart->horizontalHeader()->setVisible(false);
-    depart->verticalHeader()->setVisible(false);
-    depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setFixedSize(dimCol * taille, taille);
+	depart->horizontalHeader()->setVisible(false);
+	depart->verticalHeader()->setVisible(false);
+	depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     for(unsigned int i=0; i<dimCol; i++){
-        depart->setColumnWidth(i, taille);
-        depart->setItem(0, i, new QTableWidgetItem(""));
-    }
-
+		depart->setColumnWidth(i, taille);
+		depart->setItem(0, i, new QTableWidgetItem(""));
+	}
 }
 
-void Gameoflife::setGrid2D(){
-    QString dimensionCol=colField->text();
-    QString dimensionRow=rowField->text();
+void Gameoflife::setGrid2D()
+{
+	QString dimensionCol = colField->text();
+	QString dimensionRow = rowField->text();
 
+	int dimCol = dimensionCol.toInt();
+	int dimRow = dimensionRow.toInt();
 
-    int dimCol=dimensionCol.toInt();
-    int dimRow=dimensionRow.toInt();
-
-    depart->setColumnCount(dimCol);
-    depart->setRowCount(dimRow);
+	depart->setColumnCount(dimCol);
+	depart->setRowCount(dimRow);
 
     unsigned int taille = 25;
 
-    depart->setFixedSize(dimCol*taille, dimRow*taille);
-    depart->horizontalHeader()->setVisible(false);
-    depart->verticalHeader()->setVisible(false);
-    depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setFixedSize(dimCol * taille, dimRow * taille);
+	depart->horizontalHeader()->setVisible(false);
+	depart->verticalHeader()->setVisible(false);
+	depart->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	depart->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     for(unsigned int i=0; i<dimCol; i++){
         for(unsigned int j=0; j<dimRow; j++){
-            depart->setColumnWidth(i, taille);
-            depart->setRowHeight(j, taille);
-            depart->setItem(j, i, new QTableWidgetItem(""));
-        }
-    }
-
-
+			depart->setColumnWidth(i, taille);
+			depart->setRowHeight(j, taille);
+			depart->setItem(j, i, new QTableWidgetItem(""));
+		}
+	}
 }
--- gameoflife.h
+++ gameoflife.h
@@ -1,48 +1,44 @@
 #ifndef GAMEOFLIFE
 #define GAMEOFLIFE
 
-#include<QWidget>
-#include<QPushButton>
-#include<QHBoxLayout>
-#include<QLabel>
-#include<QLineEdit>
-#include<QTableWidget>
-#include<QHeaderView>
+#include <QHBoxLayout>
+#include <QHeaderView>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QWidget>
 
-class Gameoflife : public QWidget{
+class Gameoflife : public QWidget {
     Q_OBJECT
-    QPushButton *Automaton1D;
-    QPushButton *Automaton2D;
-    QPushButton *HexaAutomaton;
-    QHBoxLayout *presentation;
-    QLabel * introduction;
-    QVBoxLayout *presentation2;
-    QWidget * Simulation;
-    QPushButton * retour;
-    QPushButton * runSimulation;
-    QPushButton * next;
-    QPushButton *size;
-    QVBoxLayout * colone1,  *grid;
-    QLineEdit * colField, *rowField;
-    QHBoxLayout * param;
-    QTableWidget *depart;
+	QPushButton* Automaton1D;
+	QPushButton* Automaton2D;
+	QPushButton* HexaAutomaton;
+	QHBoxLayout* presentation;
+	QLabel* introduction;
+	QVBoxLayout* presentation2;
+	QWidget* Simulation;
+	QPushButton* retour;
+	QPushButton* runSimulation;
+	QPushButton* next;
+	QPushButton* size;
+    QVBoxLayout *colone1, *grid;
+    QLineEdit *colField, *rowField;
+	QHBoxLayout* param;
+	QTableWidget* depart;
     static unsigned int dimension;
 
-
-public :
-    explicit Gameoflife(QWidget* parent = nullptr);
-    QWidget* createNewWindow();
+public:
+	explicit Gameoflife(QWidget* parent = nullptr);
+	QWidget* createNewWindow();
     //~Gameoflife(){}
-private slots :
+private slots:
     void Simulation_Automaton1D();
     void Simulation_Automaton2D();
     void Simulation_HexaAutomaton();
     void retour_menu();
     void setGrid1D();
     void setGrid2D();
-
-
 };
 
 #endif // GAMEOFLIFE
-
