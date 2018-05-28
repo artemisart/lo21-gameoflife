955a75ecbdeb8cedb754e29a807ce0f4a3c9d399
Mathis Chenuet <artdevelopp@hotmail.fr>
2018-05-28 09:02:44 +0200

    clang-format Rule and simplify calcNextState

--- Rule.cpp
+++ Rule.cpp
@@ -2,7 +2,10 @@
 
 /*--------------------------------RULE 1D-------------------------------------*/
 
-Rule1D::Rule1D(const std::uint8_t i) : num(i) {}
+Rule1D::Rule1D(const std::uint8_t i)
+	: num(i)
+{
+}
 
 std::uint8_t Rule1D::getNum() const
 {
@@ -14,21 +17,21 @@ void Rule1D::setNum(const std::uint8_t i)
 	this->num = i;
 }
 
-bool Rule1D::calcNextState(const Grid<bool,Index1D> &grid, const Index1D index) const
+bool Rule1D::calcNextState(const Grid<bool, Index1D>& grid, const Index1D index) const
 {
-	int y = + 4*grid.getCell(index.offset(-1))
-			+ 2*grid.getCell(index)
-			+ 1*grid.getCell(index.offset(+1));
+	int y = +4 * grid.getCell(index.offset(-1))
+		+ 2 * grid.getCell(index)
+		+ 1 * grid.getCell(index.offset(+1));
 	return (this->num >> y) & 1;
 }
 
 /*--------------------------------RULE 2D-------------------------------------*/
 
-
-Rule2D::Rule2D(const std::uint8_t b, const std::uint8_t s) :
-	born(b),
-	survive(s)
-{}
+Rule2D::Rule2D(const std::uint8_t b, const std::uint8_t s)
+	: born(b)
+	, survive(s)
+{
+}
 
 std::uint8_t Rule2D::getBorn() const
 {
@@ -50,20 +53,17 @@ void Rule2D::setSurvive(const std::uint8_t i)
 	this->survive = i;
 }
 
-bool Rule2D::calcNextState(const Grid<bool,Index2D> &grid, const Index2D index) const
+bool Rule2D::calcNextState(const Grid<bool, Index2D>& grid, const Index2D index) const
 {
-	int neighbourCount =
-			+ grid.getCell(index.offset(-1, -1))
-			+ grid.getCell(index.offset(-1,  0))
-			+ grid.getCell(index.offset(-1, +1))
-			+ grid.getCell(index.offset( 0, -1))
-			+ grid.getCell(index.offset( 0, +1))
-			+ grid.getCell(index.offset(+1, -1))
-			+ grid.getCell(index.offset(+1,  0))
-			+ grid.getCell(index.offset(+1, +1));
+	int neighbourCount = +grid.getCell(index.offset(-1, -1))
+		+ grid.getCell(index.offset(-1, 0))
+		+ grid.getCell(index.offset(-1, +1))
+		+ grid.getCell(index.offset(0, -1))
+		+ grid.getCell(index.offset(0, +1))
+		+ grid.getCell(index.offset(+1, -1))
+		+ grid.getCell(index.offset(+1, 0))
+		+ grid.getCell(index.offset(+1, +1));
 
-	if (grid.getCell(index)) // the current cell is alive
-		return (this->survive >> neighbourCount) & 1;
-	else // the current cell is dead
-		return (this->born >> neighbourCount) & 1;
+	auto rule = grid.getCell(index) ? this->survive : this->born;
+	return (rule >> neighbourCount) & 1;
 }
