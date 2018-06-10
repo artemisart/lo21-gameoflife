fc329f573dcb0d5ffec9ec03924d16ec4999790c
Mathis Chenuet <artdevelopp@hotmail.fr>
2018-06-10 22:23:30 +0200

    automate_8d: fix copy of arrays (1 element was missing)

--- automate_2d.cpp
+++ automate_2d.cpp
@@ -20,9 +20,8 @@ Automate_2D::Automate_2D(QWidget* parent)
         ui->Born0, ui->Born1, ui->Born2, ui->Born3,
 		ui->Born4, ui->Born5, ui->Born6, ui->Born7, ui->Born8
     };
-	std::copy_n(_survive, 8, survive); // because numBits is not directly assignable
-
-	std::copy_n(_born, 8, born);
+	std::copy_n(_survive, 9, survive); // because arrays are not directly assignable
+	std::copy_n(_born, 9, born);
 
 	ui->grid->setFixedSize(ui->largeur->value() * 25, ui->hauteur->value() * 25);
     for (int i = 0; i < 20; i++) {
